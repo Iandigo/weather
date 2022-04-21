03f0288c080e4f85c95d8d2375af0c8cdccfe2c7
nguyentien <nguyentien@deakin.edu.au>
2022-04-21 16:32:24 +1000

    add weather components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import Weather from "./components/weather";
 
 const api = {
   key: "836f973ec725f3e764a5f657fd13446c",
@@ -18,39 +19,6 @@ export default function App() {
           setQuery("");
         });
     }
-  };
-
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
   };
 
   return (
@@ -75,16 +43,7 @@ export default function App() {
           />
         </div>
         {typeof weather.main != "undefined" ? (
-          <div className="location-box">
-            <div className="location">
-              {weather.name}, {weather.sys.country}
-            </div>
-            <div className="date">{dateBuilder(new Date())}</div>
-            <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°C</div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
+          <Weather weatherData={weather} />
         ) : (
           ""
         )}
